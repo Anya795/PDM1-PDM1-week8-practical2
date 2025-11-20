f5fc8f287bf201f334e0b7b3e8af5a50e674fd3d
Anya795 <psd540@york.ac.uk>
2025-11-20 11:37:49 +0000

    hey

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,17 @@
+let rawText;
+let parts = split(someString, "\t");
+
+function preload(){
+    rawText = loadStrings("/assets/imdb_labelled.txt")
+}
+
+
+function setup(){
+    createCanvas(400,400);
+
+}
+
+function draw(){
+    background(0);
+}
+
